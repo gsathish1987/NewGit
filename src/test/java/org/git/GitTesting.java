0f49c40cc870039188ba85546183efc5be2feb49
gsathish1987 <gsathish.k1987@gmail.com>
2022-04-21 10:08:15 +0530

    New Code

---
+++ src/test/java/org/git/GitTesting.java
@@ -0,0 +1,15 @@
+package org.git;
+
+import org.testng.annotations.Test;
+
+public class GitTesting {
+	
+	@Test
+	private void add() {
+		
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+	}
+
+}
